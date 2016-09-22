74d5d5f5ed9dd30f887c4df4196618ba7068f15c
GregroyRoosenboom <roosenboom.gregory@gmail.com>
2016-09-22 21:20:59 +0200

    added a dummy class and test

---
+++ Project/src/Test.java
@@ -0,0 +1,5 @@
+/**
+ * Created by surfacepro2 on 22/09/2016.
+ */
+public class Test {
+}
---
+++ Project/test/TestTest.java
@@ -0,0 +1,18 @@
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+
+/**
+ * Created by surfacepro2 on 22/09/2016.
+ */
+public class TestTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+}
