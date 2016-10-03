7181f863ae26a1e88245162652de0f42c5098159
GregroyRoosenboom <roosenboom.gregory@gmail.com>
2016-10-03 19:38:29 +0200

    try the test again

--- Project/src/Test.java
+++ Project/src/Test.java
@@ -2,4 +2,11 @@
  * Created by surfacepro2 on 22/09/2016.
  */
 public class Test {
+
+    public boolean getTrue(int i){
+        if (i < 5)
+        {return true;}
+        else
+        {return false;}
+    }
 }
--- Project/test/TestTest.java
+++ Project/test/TestTest.java
@@ -1,18 +1,16 @@
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
+import org.junit.*;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by surfacepro2 on 22/09/2016.
  */
 public class TestTest {
-    @Before
-    public void setUp() throws Exception {
-    }
+@org.junit.Test
+    public void testgetTrue(){
+    Test test =  new Test();
+    assertEquals(true, test.getTrue(3));
 
-    @After
-    public void tearDown() throws Exception {
-
-    }
+}
 
 }
